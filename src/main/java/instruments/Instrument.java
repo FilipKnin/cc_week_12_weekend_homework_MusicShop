70a35f6d0618d0c5b924bbfccf8c5556ffebe75c
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-19 14:17:38 +0100

    Initial commit. Instrument Class created

---
+++ src/main/java/instruments/Instrument.java
@@ -0,0 +1,11 @@
+package instruments;
+
+public abstract class Instrument {
+    private String brand;
+    private String model;
+    private InstrumentType type;
+    private double price;
+
+
+
+}
