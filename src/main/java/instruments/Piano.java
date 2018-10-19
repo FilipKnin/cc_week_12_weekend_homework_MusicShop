44a737d436de5243b5004078ed15747bfc3e6fe7
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-19 15:11:16 +0100

    Piano class created and tested.

--- src/main/java/instruments/Piano.java
+++ src/main/java/instruments/Piano.java
@@ -1,4 +1,15 @@
 package instruments;
 
-public class Piano {
+public class Piano extends Instrument {
+
+    private String colour;
+
+    public Piano(String brand, String model, InstrumentType type, String colour, double buyingPrice, double sellingPrice) {
+        super(brand, model, type, buyingPrice, sellingPrice);
+        this.colour = colour;
+    }
+
+    public String getColour() {
+        return this.colour;
+    }
 }
--- src/test/java/PianoTest.java
+++ src/test/java/PianoTest.java
@@ -1,2 +1,58 @@
+import instruments.InstrumentType;
+import instruments.Piano;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PianoTest {
+
+    Piano piano;
+
+    @Before
+    public void before() {
+        piano = new Piano("Brand", "Model", InstrumentType.Keyboard,"Black", 1000.00, 2000.00);
+    }
+
+    @Test
+    public void hasBrand() {
+        assertEquals("Brand", piano.getBrand());
+    }
+
+    @Test
+    public void hasModel() {
+        assertEquals("Model", piano.getModel());
+    }
+
+    @Test
+    public void hasType() {
+        assertEquals(InstrumentType.Keyboard, piano.getType());
+    }
+
+    @Test
+    public void hasColour() {
+        assertEquals("Black", piano.getColour());
+    }
+
+    @Test
+    public void hasBuyingPrice() {
+        assertEquals(1000.00, piano.getBuyingPrice(),0.01);
+    }
+
+    @Test
+    public void canSetBuyingPrice() {
+        piano.setBuyingPrice(1500.00);
+        assertEquals(1500.00, piano.getBuyingPrice(),0.01);
+    }
+
+    @Test
+    public void hasSellingPrice() {
+        assertEquals(2000., piano.getSellingPrice(),0.01);
+    }
+
+    @Test
+    public void canSetSellingPrice() {
+        piano.setSellingPrice(3000.00);
+        assertEquals(3000.00, piano.getSellingPrice(),0.01);
+    }
 }
