ef41521348d1236447e37140c6f8b3505c7e1d52
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-19 16:15:28 +0100

    Shop.countStockItems created and tested

--- src/main/java/shop/Shop.java
+++ src/main/java/shop/Shop.java
@@ -21,4 +21,8 @@ public class Shop {
     public ArrayList<ISell> getStock() {
         return this.stock;
     }
+
+    public int countStockItems() {
+        return this.stock.size();
+    }
 }
--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -1,2 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+import shop.Shop;
+
+import static org.junit.Assert.assertEquals;
+
 public class ShopTest {
+
+    Shop shop;
+
+    @Before
+    public void before() {
+        shop = new Shop("name");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("name", shop.getName());
+    }
+
+    @Test
+    public void canCountStockItems() {
+        assertEquals(0, shop.countStockItems());
+    }
+
 }
