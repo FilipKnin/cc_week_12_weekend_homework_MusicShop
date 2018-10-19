e9f9663321ba21b2c603d0ccf264f516cc03e66d
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-19 14:41:15 +0100

    Instrument class - getters and setters created

--- src/main/java/instruments/Instrument.java
+++ src/main/java/instruments/Instrument.java
@@ -1,11 +1,38 @@
 package instruments;
 
 public abstract class Instrument {
+
     private String brand;
     private String model;
     private InstrumentType type;
     private double price;
 
 
+    public Instrument(String brand, String model, InstrumentType type, double price) {
 
+        this.brand = brand;
+        this.model= model;
+        this.type = type;
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public InstrumentType getType() {
+        return this.type;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
