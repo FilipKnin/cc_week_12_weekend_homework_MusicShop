02ed12574f3f14b47468cae7122397e2ff04f692
Filip Kuszenin <filipkuszenin@gmail.com>
2018-10-19 16:12:20 +0100

    Shop class created.

---
+++ src/main/java/shop/Shop.java
@@ -0,0 +1,24 @@
+package shop;
+
+import interfaces.ISell;
+
+import java.util.ArrayList;
+
+public class Shop {
+    private String name;
+    private ArrayList<ISell> stock;
+
+    public Shop(String name) {
+
+        this.name = name;
+        this.stock = new ArrayList<>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public ArrayList<ISell> getStock() {
+        return this.stock;
+    }
+}
